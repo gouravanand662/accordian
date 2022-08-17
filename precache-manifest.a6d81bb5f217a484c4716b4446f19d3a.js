23b2320dc4f3ef0487b010979996b5154db1987c
Gourav Anand <gouravanand662@gmail.comgit config --global user.email gouravanand662@gmail.comgit config --global user.email gouravanand662@gmail.com>
2022-08-17 05:51:33 +0530

    Updates

---
+++ precache-manifest.a6d81bb5f217a484c4716b4446f19d3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52c3b3716ae15e0edfd01a7f4f800410",
+    "url": "/accordian/index.html"
+  },
+  {
+    "revision": "e3dac815a4a4f8761619",
+    "url": "/accordian/static/css/main.4b589e59.chunk.css"
+  },
+  {
+    "revision": "ab85a117f39d501e282e",
+    "url": "/accordian/static/js/2.40b1d22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/accordian/static/js/2.40b1d22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3dac815a4a4f8761619",
+    "url": "/accordian/static/js/main.90a2aa92.chunk.js"
+  },
+  {
+    "revision": "b9012adf2d0eb94649e5",
+    "url": "/accordian/static/js/runtime-main.af1fad76.js"
+  }
+]);
